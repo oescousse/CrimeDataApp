708b6c4ea31bf13dec84a4d0ba150c46702ad2fa
Ryan Friedman <rdfriedman99@gmail.com>
2020-07-03 21:59:47 -0400

    navbar successfully routes to home, about, register, login and listings pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+//basic react app imports
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//navbar imports
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
+
+//import other pages
+import { Home } from './Home';
+import { About } from './About';
+import { Login } from './Login';
+import { Logout } from './Logout';
+import { Register } from './Register';
+import { Listings } from './Listings';
+import { NoMatch } from './NoMatch';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/listings" component={Listings} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
