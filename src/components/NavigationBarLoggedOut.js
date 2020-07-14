0a529e4aa945fb07922cf90af2cf64a29f48dfca
Ryan Friedman <rdfriedman99@gmail.com>
2020-07-13 20:50:29 -0400

    separated out css from navigation bar files

--- src/components/NavigationBarLoggedOut.js
+++ src/components/NavigationBarLoggedOut.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
-import styled from 'styled-components';
+import Styles from './NavigationBarStyles';
 
-const Styles = styled.div`
-  .navbar { background-color: #DEB887; }
-  a, .navbar-nav, .navbar-light .nav-link {
-    color: #000766;
-    &:hover { color: white; }
-  }
-  .navbar-brand {
-    font-size: 1.4em;
-    color: #000766;
-    &:hover { color: white; }
-  }
-  .form-center {
-    position: absolute !important;
-    left: 25%;
-    right: 25%;
-  }
-`;
 export const NavigationBarLoggedOut = () => (
   <Styles>
     <Navbar expand="lg">
