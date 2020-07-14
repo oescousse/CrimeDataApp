3d5b3dff5031afcf6cfcf0c5605ec41950e46016
Ryan Friedman <rdfriedman99@gmail.com>
2020-07-14 19:41:32 -0400

    changed nav bar to be class components

--- src/components/NavigationBar/LoggedIn/NavigationBarLoggedIn.js
+++ src/components/NavigationBar/LoggedIn/NavigationBarLoggedIn.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import Styles from '../NavigationBarStyles';
 
-export const NavigationBarLoggedIn = () => (
-  <Styles>
-    <Navbar expand="lg">
-      <Navbar.Brand href="/">HomeAwayCrime</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
+export class NavigationBarLoggedIn extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render () {
+    return (
+    <Styles>
+      <Navbar expand="lg">
+        <Navbar.Brand href="/">HomeAwayCrime</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
           <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
           <Nav.Item><Nav.Link href="/listings">Listings</Nav.Link></Nav.Item> 
           <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
           <Nav.Item><Nav.Link href="/logout">Logout</Nav.Link></Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </Styles>
-)
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Styles>
+    )
+  };
+}
