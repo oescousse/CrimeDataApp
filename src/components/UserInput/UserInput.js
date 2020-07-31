fcb932749b5b2233760ec96b5de7cb55443430dc
oescousse <60245239+oescousse@users.noreply.github.com>
2020-07-31 18:48:53 -0400

    added user input component

---
+++ src/components/UserInput/UserInput.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+    Container, Col, Form,
+    FormGroup, Label, Input,
+    Button,
+  } from 'reactstrap';
+import './UserInput.css';
+
+class UserInput extends Component {
+    render() {
+        return (
+            <Container className="UserInput">
+                <h2>Search</h2>
+                <Form className="form">
+                    <Col>
+                        <FormGroup>
+                            <Label>Zip Code:</Label>
+                            <Input
+                            type="number"
+                            name="number"
+                            id="exampleNumber"
+                            placeholder="00000"
+                            />
+                        </FormGroup>
+                    </Col>
+                </Form>
+            </Container>
+        );
+    }
+}
+
+export default UserInput;
