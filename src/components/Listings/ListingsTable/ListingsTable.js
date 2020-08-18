91c4974efa2c719fcd20b7ea73b602c0ef1dc7c7
Ryan Friedman <rdfriedman99@gmail.com>
2020-08-17 21:17:46 -0400

    simplified table implimentation, still need new pagination

--- src/components/Listings/ListingsTable/ListingsTable.js
+++ src/components/Listings/ListingsTable/ListingsTable.js
@@ -1,104 +1,73 @@
 import React from 'react';
-import { useTable, usePagination } from 'react-table'
-import "./Pagination.css"
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-export function Table({columns, data}) {
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    prepareRow,
-    page,
-    canPreviousPage,
-    canNextPage,
-    pageOptions,
-    pageCount,
-    gotoPage,
-    nextPage,
-    previousPage,
-    setPageSize,
-    state: { pageIndex, pageSize },
-  } = useTable(
-    {
-      columns,
-      data,
-      initialState: { pageIndex: 0 },
-    },
-    usePagination
-  )
+const useStyles = makeStyles({
+  table: {
+    verticalAlign: 'middle'
+  },
+  root: {
+    width: '208%',
+    // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+  }
+});
 
-  return (
-    <>
-       <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map((row, i) => {
-            prepareRow(row)
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                })}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>  
-
-     <div className="pagination">
-        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-          {'First Page'}
-        </button>{' '}
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          {'<'}
-        </button>{' '}
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          {'>'}
-        </button>{' '}
-        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-          {'Last Page'}
-        </button>{' '}
-        <span>
-          Page{' '}
-          <strong>
-            {pageIndex + 1} of {pageOptions.length}
-          </strong>{' '}
-        </span>
-        <span>
-          | Go to page:{' '}
-          <input
-            type="number"
-            defaultValue={pageIndex + 1}
-            onChange={e => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              gotoPage(page)
-            }}
-          />
-        </span>{' '}
-        <select
-          value={pageSize}
-          onChange={e => {
-            setPageSize(Number(e.target.value))
-          }}
-        >
-          {[5, 10, 25].map(pageSize => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize} rows
-            </option>
-          ))}
-        </select>
-      </div>
-    </>
-
-  )
+function createData(address, neighborhood, rating, link) {
+  return { address, neighborhood, rating, link };
 }
 
-export default Table;
+const rows = [
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+  createData('417 E 57th St, New York', 'Sutton Place', "####", <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>),
+];
+
+export default function DataTable() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer className={classes.root} elevation={3} component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Address</TableCell>
+            <TableCell align="right">Neighborhood</TableCell>
+            <TableCell align="right">Crime Rating</TableCell>
+            <TableCell align="right">Link</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.address}
+              </TableCell>
+              <TableCell align="right">{row.neighborhood}</TableCell>
+              <TableCell align="right">{row.rating}</TableCell>
+              <TableCell align="right">{row.link}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/components/Listings/ListingsTabs/RentalListings.js
+++ src/components/Listings/ListingsTabs/RentalListings.js
@@ -1,116 +1,12 @@
 import React, { Component } from 'react';
-import { Table } from "../ListingsTable/ListingsTable";
-import Styles from "../ListingsTable/ListingsTableStyles";
+import DataTable from "../ListingsTable/ListingsTable";
+// import Styles from "../ListingsTable/ListingsTableStyles";
 
 class RentalListings extends Component {
 
   render(){
-    const data = [
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      },
-      {
-        address: '417 E 57th St, New York',
-        neighborhood: 'Sutton Place',
-        rating: "####",
-        link: <a href="https://www.realtor.com/realestateandhomes-detail/417-E-57th-St_New-York_NY_10022_M93827-80821">realtor.com</a>
-      }
-    ]
-  
-    const columns = [
-      {
-        Header: 'Address',
-        accessor: 'address'
-      }, {
-        Header: 'Neighborhood',
-        accessor: 'neighborhood'
-      }, {
-        Header: 'Crime Rating',
-        accessor: 'rating'
-      }, {
-        Header: 'External Link',
-        accessor: 'link'
-      }
-    ]
     return (
-      <Styles>
-        <Table data={data} columns={columns}/>
-      </Styles>
+      <div><DataTable/></div>
     )
   }
 }
