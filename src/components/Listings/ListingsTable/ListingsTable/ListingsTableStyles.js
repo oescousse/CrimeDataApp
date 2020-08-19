b880ec953cc36c0a339fd323cc2fc17047bba0c9
Ryan Friedman <rdfriedman99@gmail.com>
2020-08-19 14:47:13 -0400

    fixed double export

--- src/components/Listings/ListingsTable/ListingsTable/ListingsTableStyles.js
+++ src/components/Listings/ListingsTable/ListingsTable/ListingsTableStyles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles'
 
-export const tableStyles = makeStyles({
+const tableStyles = makeStyles({
   table: {
     minWidth: 500,
   },
--- src/components/Listings/ListingsTable/ListingsTablePagination/PaginationStyle.js
+++ src/components/Listings/ListingsTable/ListingsTablePagination/PaginationStyle.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles'
 
-export const paginationStyles = makeStyles((theme) => ({
+const paginationStyles = makeStyles((theme) => ({
   root: {
     flexShrink: 0,
     marginLeft: theme.spacing(2.5),
