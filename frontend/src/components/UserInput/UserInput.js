9867f83b987c67fe6be48df1262d98f537249a48
oescousse <60245239+oescousse@users.noreply.github.com>
2020-09-09 13:53:28 -0400

    added API call

--- frontend/src/components/UserInput/UserInput.js
+++ frontend/src/components/UserInput/UserInput.js
@@ -6,36 +6,49 @@ import {
 import './UserInput.css';
 
 export class UserInput extends Component {
-
-    constructor(props) {
+constructor(props) {
         super(props);
-        this.state = { isLoaded: false, zip: '', data: null };
+        this.state = { isLoaded: false, zip: '', lat: '', lng: '', data: null };
+        this.handleChange = this.handleChange.bind(this);
         this.callApi = this.callApi.bind(this);
-        this.handleChange = this.handleChange.bind(this)
     }
-
+    
     handleChange(event) { this.setState({ zip: event.target.value }); }
 
     callApi() {
-        console.log(this.state)
+
+        fetch("https://www.zipcodeapi.com/rest/info.json/" + this.state.zip)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ lat: data.lat, lng: data.lng })
+                console.log(data);
+            })
+            .catch(console.log)
     }
+
+
     render() {
         return (
             <Container className="UserInput">
-                <Col></Col>
-                <Col sm={{ size: 6, offset: 3 }}>
-                <InputGroup>
-                    <InputGroupAddon addonType="prepend">
-                        <Button color="info" onClick={this.callApi}>Search</Button>
-                    </InputGroupAddon>
-                    <Input type="text"
-                        value={this.state.zip}
-                        maxLength="5"
-                        placeholder="Zip Code"
-                        onChange={this.handleChange} />
-                </InputGroup>
-                </Col>
-                <Col></Col>
+                <Form className="form" inline>
+                    <Col ></Col>
+                        <FormGroup>
+                            <Input
+                            type="text"
+                            maxLength="5"
+                            onInput={this.maxLengthCheck}
+                            name="zipCode"
+                            id="zipCode"
+                            placeholder="Enter Zip Code"
+                            value={this.state.zip}
+                            onChange={this.handleChange}
+                            />
+                        </FormGroup>
+                        <Button onClick={this.callApi} color="secondary">Search</Button>
+                        <h3>Latitude value: {this.state.lat}</h3>
+                        <h3>Longitude value: {this.state.lng}</h3>
+                        <Col ></Col>
+                </Form>
             </Container>
         );
     }
